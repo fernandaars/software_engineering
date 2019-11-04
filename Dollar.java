3f6cfbb6d4c9ec967911eec7eda0b29d249b07b9
fernandaars <fernandaaprsilva@gmail.com>
2019-11-04 14:09:24 -0200

    Forth Commit: Updating Dollar, Money and Franc classes

--- Dollar.java
+++ Dollar.java
@@ -1,25 +1,44 @@
-class Money {
-   ...
+abstract class Money {
+   private String currency; 
+
    static Money dollar(int amount)  {
-      return new Dollar(amount);
+      return new Dollar(amount, "USD");
    }
-   
+
    static Money franc(int amount) {
-      return new Franc(amount);
-    }
-} 
+      return new Franc(amount, "CHF");
+   }
 
-class Dollar {
-   Money times(int multiplier)  {
-      return new Dollar(amount * multiplier);
-   }								
-}    
+   Money(int amount, String currency) {
+      this.amount = amount;
+      this.currency = currency;
+   }
+}
 
-class Franc {
+class Franc extends Money {	
+   Franc(int amount, String currency) {
+      super(amount, currency);
+   }
+     
    Money times(int multiplier)  {
-      return new Franc(amount * multiplier);
-   }								
-}   
+      return Money.franc(amount * multiplier);
+   }
+}
+
+class Dollar extends Money {	
+   Dollar(int amount, String currency)  {
+      super(amount, currency);
+   }
+	
+   Money times(int multiplier)  {
+      return Money.dollar(amount * multiplier);
+   }
+}
+
+public void testCurrency() {
+   assertEquals("USD", Money.dollar(1).currency());
+   assertEquals("CHF", Money.franc(1).currency());
+}
 
 public boolean equals(Object object)  {
    Dollar dollar = (Dollar) object;
